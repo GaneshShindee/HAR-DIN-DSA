670089cc3068575f46a612b0168686601bafa92b
Ganesh Shinde <ganeshshinde0100@gmail.com>
2024-07-31 09:57:11 +0530

    video no 1

---
+++ 2-FirstCode/aPrintfunction.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+                  
+int main(){
+    cout<<"Namaste Duniya"<<endl;
+    cout<<"Main love bapper Hu"<<endl;
+    cout<<"mai bahut ameer hu"<<endl;
+    cout<<"tu vhutiya hain re"<<endl;
+    cout<<"kya kar raha hain re bhai"<<endl;
+    cout<<"mar ja bhai tu abhi"<<endl;
+    return 0;
+}
---
+++ 2-FirstCode/bDataTypes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+                  
+int main(){
+    int a =5;
+    char b = 'a';
+    string c= "abc";
+    bool d =true;
+     double e=5.22;
+    float f=3.265;
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b)<<endl;
+    cout<<sizeof(c)<<endl;
+    cout<<sizeof(d)<<endl;
+    cout<<sizeof(e)<<endl;
+    cout<<sizeof(f)<<endl;
+
+    return 0;
+}
