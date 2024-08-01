cfb6ca043ef5cada4c48bb01820bca194ac2c8c3
Ganesh Shinde <ganeshshinde0100@gmail.com>
2024-08-01 21:34:29 +0530

    starting video no 5

---
+++ 005-bitwise-operator/001bitwiseOperator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+                  
+int main(){
+    int a = 4;
+    int b=5;
+    cout<<"a&b is "<<(a&b)<<endl;
+    cout<<"a|b is "<<(a|b)<<endl;
+    cout<<"~a is "<<(~a)<<endl;
+    cout<<"a^b is "<<(a^b)<<endl;
+    return 0;
+}
