da389a0892dc705f98a3a235ecc9c28b53b370ea
Ganesh Shinde <ganeshshinde0100@gmail.com>
2024-08-07 06:42:47 +0530

    g

--- 009-InroToArray/PwSkiils College Wallah/001-vectors.cpp
+++ 009-InroToArray/PwSkiils College Wallah/001-vectors.cpp
@@ -1,7 +1,41 @@
 #include<iostream>
+#include<vector>
 using namespace std;
                   
 int main(){
+    vector<int>arr;
+    cout<<arr.size()<<endl;
+    cout<<arr.capacity()<<endl;
+    arr.push_back(1);
+    cout <<endl;
+
+    cout<<arr.size()<<endl;
+    cout<<arr.capacity()<<endl;
+    arr.push_back(2);
+    cout <<endl;
+
+    cout<<arr.size()<<endl;
+    cout<<arr.capacity()<<endl;
+    arr.push_back(3);
+    cout <<endl;
+
+    cout<<arr.size()<<endl;
+    cout<<arr.capacity()<<endl;
+    arr.push_back(4);
+    cout <<endl;
+
+    cout<<arr.size()<<endl;
+    cout<<arr.capacity()<<endl;
+    arr.push_back(5);
+    cout<< endl;;
+
+    cout<<arr.size()<<endl;
+    cout<<arr.capacity()<<endl;
+    arr.push_back(6);
+    cout <<endl;
+
+    
+    
     
     return 0;
 }
