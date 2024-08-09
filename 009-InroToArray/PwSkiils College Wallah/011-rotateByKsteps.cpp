f52c9068f1a333510a468bb4b4548ca68b7801cc
Ganesh Shinde <ganeshshinde0100@gmail.com>
2024-08-09 07:28:49 +0530

    .

--- 009-InroToArray/PwSkiils College Wallah/011-rotateByKsteps.cpp
+++ 009-InroToArray/PwSkiils College Wallah/011-rotateByKsteps.cpp
@@ -4,21 +4,22 @@ using namespace std;
 int main(){
     int arr[]={1,2,3,4,5};
     int n=5;
-    int k=2;
+    int k;
+    cin>>k;
     // k should be less than n;
     k=k%n;
     int ansarr[5];
     int j=0;
     for(int i=n-k;i<n;i++){
-        ansarr[j]=arr[i];
-        j++; 
+        ansarr[j++]=arr[i];
+        // j++; 
     }
 
-    for(int i=0;i<n;i++){
-        arr[i]=arr[j++];
+    for(int i=0;i<k;i++){
+        ansarr[j++]=arr[i];
     }
     for(int i=0;i<n;i++){
-        cout<<arr[i] <<" ";
+        cout<<ansarr[i] <<" ";
     }
 
     return 0;
