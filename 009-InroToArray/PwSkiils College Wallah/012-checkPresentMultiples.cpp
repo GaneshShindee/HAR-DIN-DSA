6f7bf792d0198888a7e6842d0f9b7b1fb7fd434c
Ganesh Shinde <ganeshshinde0100@gmail.com>
2024-08-09 08:08:14 +0530

    .

--- 009-InroToArray/PwSkiils College Wallah/012-checkPresentMultiples.cpp
+++ 009-InroToArray/PwSkiils College Wallah/012-checkPresentMultiples.cpp
@@ -9,6 +9,22 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>v[i];
     }
+
+    const int N=1e5 + 10;
+    vector<int>freq(N,0);
+    for(int i=0;i<n;i++){
+        freq[v[i]]++;
+    }
+    cout<<"enter no of  querys:-" <<endl;
+
+    int q;
+    cin>>q;
+    while(q--){
+        int element;
+        cin>>element;
+        cout<<freq[element] <<endl;
+    }
+
     
     return 0;
 }
