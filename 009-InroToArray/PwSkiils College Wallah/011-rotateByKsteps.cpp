75c5411e3e5d68e2b6b6a0d9c04918120ecdba70
Ganesh Shinde <ganeshshinde0100@gmail.com>
2024-08-09 07:14:38 +0530

    .

---
+++ 009-InroToArray/PwSkiils College Wallah/011-rotateByKsteps.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+                  
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=5;
+    int k=2;
+    // k should be less than n;
+    k=k%n;
+    for(int i=0;i<n;i++){
+        
+    }
+
+    return 0;
+}
