a2d2f18fe804d791ef5923fd2d5f675d9cce3b17
Ganesh Shinde <ganeshshinde0100@gmail.com>
2024-08-10 07:28:28 +0530

    .

--- 009-InroToArray/PwSkiils College Wallah/019-SortedArrayGiveSquareArray.cpp
+++ 009-InroToArray/PwSkiils College Wallah/019-SortedArrayGiveSquareArray.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 void squareSorted(vector<int>&v){
+    for(int i=0;i<v.size();i++){
+        int ele=v[i];
+        v[i]=ele*ele;
+    }
+    for(int i=0;i<v.size();i++){
+        for(int j=i;j<v.size();j++){
+            if(v[i]>v[j]){
+                swap(v[i],v[j]);
+            }
+        }
+    }
     
 }
                   
