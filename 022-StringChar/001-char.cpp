0924766d3b8701091cdbd38fb5d6e0a29849b7a4
Ganesh Shinde <ganeshshinde0100@gmail.com>
2024-08-27 08:24:28 +0530

    .

---
+++ 022-StringChar/001-char.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+                  
+int main(){
+    char ch1 ='h';
+    int position =ch1-'a';
+    cout<< position <<endl; 
+
+    return 0;
+}
---
+++ 022-StringChar/002-charArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+                  
+int main(){
+    
+    char str [6]={'a','b','c','d','e','\0'};
+    cout<< str[0] <<endl; 
+    cout<< str[1] <<endl; 
+    cout<< str[2] <<endl; 
+    cout<< str[3] <<endl; 
+    cout<< str[4] <<endl; 
+    cout<< str <<endl; 
+
+    char str1 [13]={'G','a','n','e','s','h',' ','S','h','i','n','d','e'};
+    cout<< str1; 
+    
+    return 0;
+
+}
