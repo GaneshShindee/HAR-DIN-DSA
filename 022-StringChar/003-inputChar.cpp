cba9f07528e23b8dcaa5bf5e379b124d1458f720
Ganesh Shinde <ganeshshinde0100@gmail.com>
2024-08-28 06:35:23 +0530

    .

---
+++ 022-StringChar/003-inputChar.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+                  
+int main(){
+    char word[10];
+    cin>>word;
+
+    cout<< word <<endl; 
+    cout<< strlen(word) <<endl; 
+
+    return 0;
+}
