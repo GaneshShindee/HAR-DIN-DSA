cab3b6edf1f983e87e17279984fa488b4d1a2b82
Ganesh Shinde <ganeshshinde0100@gmail.com>
2024-08-28 07:20:13 +0530

    .

--- 022-StringChar/004-UpperCase.cpp
+++ 022-StringChar/004-UpperCase.cpp
@@ -15,10 +15,24 @@ void toUpper(char word[],int n){
     }
     cout<< word <<endl; 
 }
+
+void toLower(char word[],int n){
+    for(int i=0;i<n;i++){
+        char ch =word[i];
+        if(ch >='a' && ch<='z'){
+            continue;
+        }
+        else{
+            word[i]=ch-'A'+'a';
+        }
+    }
+    cout<< word <<endl; 
+}
                   
 int main(){
     char word[10]="Ganeshz";
     toUpper(word,strlen(word));
+    toLower(word,strlen(word));
 
 
 
