19bd432b48158f21b38b1d484bec3eb202b93634
Ganesh Shinde <ganeshshinde0100@gmail.com>
2024-09-28 12:29:48 +0530

    .

---
+++ 034-RecursionProblems/006-BubbleSort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+
+
+void printArray(int arr[],int size){
+    for (int i=0;i<size;i++){
+        cout<< arr[i] <<" "; 
+    }
+}
+void bubbleSort(int arr[],int size){
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    printArray(arr,size);
+}
+                  
+int main(){
+    int arr[]={1,0,2,6,4,5};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr,size);
+    cout <<endl; 
+
+    printArray(arr,size-1);
+    return 0;
+}
